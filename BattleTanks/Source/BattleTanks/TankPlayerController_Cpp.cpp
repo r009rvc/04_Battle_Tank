76d5db214bb57e1ecb582a1990a545ba46569e70
DESKTOP-CJ63L0O\brewe <brewerjl2468@gmail.com>
2018-05-31 10:31:41 -0400

    BT024 Creating an out Parameter Method

--- BattleTanks/Source/BattleTanks/TankPlayerController_Cpp.cpp
+++ BattleTanks/Source/BattleTanks/TankPlayerController_Cpp.cpp
@@ -37,4 +37,19 @@ void ATankPlayerController_Cpp::AimTowardsCrosshair()
 {
 	if (!GetControlledTank()) { return;}
 
+	FVector HitLocation; //Out Parameter
+	
+	if (GetSightRayHitLocation(HitLocation)) // Has "side-effect", is going to line trace 
+	{
+	UE_LOG(LogTemp, Warning, TEXT("HitLoaction: %s"), *HitLocation.ToString());
+// TODO Tell controlled tank to aim at this point 
+	}
+
+}
+// Get world location of linetrace through crosshair, true if hits landscape 
+bool ATankPlayerController_Cpp::GetSightRayHitLocation(FVector& HitLocation) const
+{
+HitLocation = FVector(1.0);
+
+return true;
 }
